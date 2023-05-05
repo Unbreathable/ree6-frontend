e6cb4eeecdbe402f6a5f3566a34f1d4a08bd213c
Julian <70802809+Unbreathable@users.noreply.github.com>
2023-05-05 17:33:23 +0200

    Error pages and start on login/register routes

---
+++ src/routes/(main)/+layout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  
+  </script>
+  <div class="body">
+  
+      <div class="content">
+          <slot />
+      </div>
+  </div>
+  
+  <style lang="scss">
+      .body {
+          padding: 0;
+          margin: 0;
+          display: flex;
+          height: 100vh;
+          width: 100vw;
+          background-color: var(--onyx);
+      }
+  
+      .content {
+          width: 100%;
+          height: calc(100% - 4rem);
+          padding: 2rem;
+      }
+  </style>
